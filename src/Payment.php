c8c02221a8a73cefc93ca5a85abbf6a0e90382d7
Jan Trejbal <jan.trejbal@gmail.com>
2016-08-29 11:10:00 +0200

    Update to ThePay-lib 3.1.0

--- src/Payment.php
+++ src/Payment.php
@@ -51,19 +51,6 @@ class Payment extends Tp\Payment
 		parent::setBackToEshopUrl($backToEshopUrl);
 	}
 
-	public function getArgs()
-	{
-		$args = parent::getArgs();
-
-		foreach ($args as &$arg) {
-			if (is_float($arg) || is_double($arg)) {
-				$arg = str_replace(',', '.', (string)$arg);
-			}
-		}
-
-		return $args;
-	}
-
 	public function getRedirectUrl()
 	{
 		$queryArgs = $this->getArgs();
