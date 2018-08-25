74550984dbf7673e98891ae932481454023ac2af
Jan Trejbal <jan.trejbal@gmail.com>
2018-08-25 19:01:22 +0200

    Fix code style

--- src/Helper/IPaymentMethod.php
+++ src/Helper/IPaymentMethod.php
@@ -1,9 +1,10 @@
-<?php
-declare(strict_types = 1);
+<?php declare(strict_types = 1);
 
 namespace Contributte\ThePay\Helper;
 
 interface IPaymentMethod
 {
+
 	public const CREDIT_CARD_PAYMENT_ID = 21;
+
 }
